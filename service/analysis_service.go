eafdd5fd04c33c8df0e92ec1473cb6f9d2d08470
BintangKantata <52192720+BintangKantata@users.noreply.github.com>
2026-02-24 23:17:30 +0700

    Add files via upload

---
+++ service/analysis_service.go
@@ -0,0 +1,37 @@
+package service
+
+import (
+	"encoding/json"
+
+	"GrowEasy/config"
+	"GrowEasy/dto"
+	"GrowEasy/models"
+
+	"gorm.io/datatypes"
+)
+
+type AnalysisService struct{}
+
+func NewAnalysisService() *AnalysisService {
+	return &AnalysisService{}
+}
+
+func (s *AnalysisService) Create(userID string, req dto.CreateAnalysisRequest) error {
+
+	soilJSON, _ := json.Marshal(req.SoilData)
+	weatherJSON, _ := json.Marshal(req.WeatherData)
+	predictionJSON, _ := json.Marshal(req.Predictions)
+
+	analysis := models.Analysis{
+		UserID:       &userID,
+		Latitude:     req.Latitude,
+		Longitude:    req.Longitude,
+		LocationName: req.LocationName,
+		SoilData:     datatypes.JSON(soilJSON),
+		WeatherData:  datatypes.JSON(weatherJSON),
+		Predictions:  datatypes.JSON(predictionJSON),
+		AiResponse:   req.AiResponse,
+	}
+
+	return config.DB.Create(&analysis).Error
+}
